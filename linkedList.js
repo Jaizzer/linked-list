8501f6a845ebf15cf00d266f7836559c783d46ac
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-27 08:02:35 +0800

    feat: Add the prepend method

--- linkedList.js
+++ linkedList.js
@@ -22,4 +22,22 @@ class LinkedList {
         this.tail = Node;
         this.size++;
     }
+
+    prepend(Node) {
+        if (this.head === null) {
+            // If the head is null, the linked list has no node, therefore the prepended node should be both the head and tail.
+            this.head = Node;
+            this.tail = Node;
+        } else {
+            // Store current head to temp.
+            temp = this.head;
+
+            // Replace current head with new Node.
+            this.head = Node;
+
+            // Point new head to the former head.
+            this.head.nextNode = temp;
+        }
+        this.size++;
+    }
 }
